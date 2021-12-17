f7d181e65545d585e918dde3fd963ab053bd848e
Wasi-Khalid <aman.wasi01@gmail.com>
2021-12-18 00:05:24 +0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {PaymentComponent} from "./components/payment/payment.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [{
+      path: '',
+      redirectTo: 'payment',
+      pathMatch: "full"
+    }, {
+      path: 'payment',
+      component: PaymentComponent
+    },
+    ]
+  }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
